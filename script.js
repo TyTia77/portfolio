8fa36510f8e2f68b16442abd597be0242b699d6e
Ty Tia <tytia77@gmail.com>
2017-05-28 23:09:01 +1000

    added links

--- script.js
+++ script.js
@@ -1,40 +1,14 @@
-$('ul li').on('click', function(e) {
-    $('ul li').removeClass('active');
-    $(this).addClass('active');
-    var test = $(this).children('a').attr('href');
-    e.preventDefault();
-
-    $('html, body').animate({
-        'scrollTop': $(test).offset().top
-    }, 1000);
-
-});
-
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 100) {
-        $('.navbar').css({
-            'background-color': 'rgba(255,255,255,.8)'
-        });
-    } else {
-        $('.navbar').css({
-            'background-color': 'rgba(255,255,255,0)'
-        });
-    }
-
-});
-
 var app = angular.module('app', []);
-
 app.controller('ctrl', ['$scope', function($scope){
     $scope.projects = [
-        {img: 'Capture.PNG', title: 'simon game'},
-        {img: 'Capture1.PNG', title: 'tic tac toe game', color: '#162924'},
-        {img: 'Capture2.PNG', title: 'quote generater', color: '#E74C3C'},
-        {img: 'Capture3.PNG', title: 'twitch stream viewer'},
-        {img: 'Capture4.PNG', title: 'weather app'},
-        {img: 'Capture5.PNG', title: 'wikipedia search', color: '#092B40'},
-        {img: 'Capture6.PNG', title: '', color: '#2C3E50'},
-        {img: 'Capture7.PNG', title: 'calculator'}
+        {img: 'images/Capture.PNG', title: 'simon game', id: 'simon-game'},
+        {img: 'images/Capture1.PNG', title: 'tic tac toe game', id: 'tictactoe', color: '#162924'},
+        {img: 'images/Capture2.PNG', title: 'quote generater', id: 'quotes', color: '#E74C3C'},
+        {img: 'images/Capture3.PNG', title: 'twitch stream viewer', id: 'twitch-streams'},
+        {img: 'images/Capture4.PNG', title: 'weather app', id: 'weatherapp'},
+        {img: 'images/Capture5.PNG', title: 'wikipedia search', id: 'wiki-viewer', color: '#092B40'},
+        {img: 'images/Capture6.PNG', title: 'pomodoro clock', id: 'pomodoro-clock', color: '#2C3E50'},
+        {img: 'images/Capture7.PNG', title: 'calculator', id: 'calculator'}
     ];
 
     $scope.mouseIn = function(e){
@@ -45,5 +19,9 @@ app.controller('ctrl', ['$scope', function($scope){
         e.target.classList.remove('show');
     }
 
-
+    $(document).on('mouseenter', '.hover-detail', function(){
+        $(this).addClass('show');
+    }).on('mouseleave', '.hover-detail', function(){
+        $(this).removeClass('show');
+    })
 }]);
