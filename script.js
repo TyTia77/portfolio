0ae8cc0036d7dac53f8a92d0c230c1cec8d5fc0a
Ty Tia <tytia77@gmail.com>
2017-05-29 21:59:08 +1000

    ui changes simple

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var app = angular.module('app', []);
 app.controller('ctrl', ['$scope', function($scope){
     $scope.projects = [
-        {img: 'images/Capture.PNG', title: 'simon game', id: 'simon-game'},
+        {img: 'images/Capture.PNG', title: 'simon game', id: 'simon-game', color: '#fff'},
         {img: 'images/Capture1.PNG', title: 'tic tac toe game', id: 'tictactoe', color: '#162924'},
         {img: 'images/Capture2.PNG', title: 'quote generater', id: 'quotes', color: '#E74C3C'},
-        {img: 'images/Capture3.PNG', title: 'twitch stream viewer', id: 'twitch-streams'},
-        {img: 'images/Capture4.PNG', title: 'weather app', id: 'weatherapp'},
+        {img: 'images/Capture3.PNG', title: 'twitch stream viewer', id: 'twitch-streams', color: '#fff'},
+        {img: 'images/Capture4.PNG', title: 'weather app', id: 'weatherapp', color: '#fff'},
         {img: 'images/Capture5.PNG', title: 'wikipedia search', id: 'wiki-viewer', color: '#092B40'},
         {img: 'images/Capture6.PNG', title: 'pomodoro clock', id: 'pomodoro-clock', color: '#2C3E50'},
-        {img: 'images/Capture7.PNG', title: 'calculator', id: 'calculator'}
+        {img: 'images/Capture7.PNG', title: 'calculator', id: 'calculator', color: '#fff'}
     ];
 
     $scope.mouseIn = function(e){
