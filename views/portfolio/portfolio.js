6e96d84547ccaaf0fde9c55e4ca979bde5c7b0c0
Ty Tia <tytia77@gmail.com>
2017-05-31 21:12:00 +1000

    test tap on mobile

--- views/portfolio/portfolio.js
+++ views/portfolio/portfolio.js
@@ -40,12 +40,12 @@ $(document).ready(function() {
 
     if (mobileAndTabletcheck()){
 
-        $(document).on('click', 'body', function() {
+        $(document).on('click touchstart', 'body', function() {
             $('.hover-detail').removeClass('show');
             $('.hover-detail').children('h3').children('a').css('pointer-events', 'none');
         })
 
-        $(document).on('click', '.hover-detail', function(e) {
+        $(document).on('click touchstart', '.hover-detail', function(e) {
             e.stopPropagation();
             $(this).addClass('show');
             $(this).children('h3').children('a').css('pointer-events', 'all');
