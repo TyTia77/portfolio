fb5f8ccf5c47ac137f9a05910c6cfc76a971f229
Ty Tia <tytia77@gmail.com>
2017-06-03 14:20:10 +1000

    test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,50 @@
 var app = angular.module('app', []);
-app.controller('ctrl', ['$scope', '$timeout', function($scope, $timeout){
+
+app.directive('scrollEffect', ['$timeout', function($timeout){
+    return {
+        link: function(){
+            $timeout(function(){
+                console.log('gg');
+                console.log($('.squares'));
+
+                window.sr = ScrollReveal({
+                    reset: true
+                });
+
+                sr.reveal('.portfolio-header', {
+                    origin: 'top',
+                    viewFactor: .1,
+                    distance: '100px',
+                    opacity: 0,
+                    duration: 700,
+                    scale: .1,
+                    mobile: true
+                })
+
+                sr.reveal('.squares', {
+                    origin: 'left',
+                    viewFactor: .3,
+                    opacity: 0,
+                    duration: 700,
+                    scale: .1,
+                    mobile: true
+                })
+
+                sr.reveal('.project-card>h3', {
+                    origin: 'bottom',
+                    viewFactor: .01,
+                    distance: '50px',
+                    opacity: 0,
+                    delay: 300,
+                    duration: 800,
+                    mobile: true
+                })
+            }, 0);
+        }
+    }
+}]);
+
+app.controller('ctrl', ['$scope', function($scope){
     // $scope.projects = [
     //     {img: 'images/Capture.PNG', title: 'simon game', id: 'simon-game', color: '#fff'},
     //     {img: 'images/Capture1.PNG', title: 'tic tac toe game', id: 'tictactoe', color: '#162924'},
@@ -18,44 +63,5 @@ app.controller('ctrl', ['$scope', '$timeout', function($scope, $timeout){
         {img: 'images/Capture5.PNG', title: 'wikipedia search', id: 'wiki-viewer', color: '#092B40'},
         {img: 'images/Capture4.PNG', title: 'weather app', id: 'weatherapp', color: '#0262B6'}
     ];
-
-    $timeout(function(){
-        console.log('gg');
-        console.log($('.squares'));
-        window.sr = ScrollReveal({
-            reset: true
-        });
-
-        sr.reveal('.portfolio-header', {
-            origin: 'top',
-            viewFactor: .1,
-            distance: '100px',
-            opacity: 0,
-            duration: 700,
-            scale: .1,
-            mobile: true
-        })
-
-        sr.reveal('.squares', {
-            origin: 'left',
-            viewFactor: .3,
-            opacity: 0,
-            duration: 700,
-            scale: .1,
-            mobile: true
-        })
-
-        sr.reveal('.project-card>h3', {
-            origin: 'bottom',
-            viewFactor: .01,
-            distance: '50px',
-            opacity: 0,
-            delay: 300,
-            duration: 800,
-            mobile: true
-        })
-    }, 0);
-
-
 
 }]);
