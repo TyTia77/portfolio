61e55b599d50724c01114e6c15c215174dae7aec
Tia <ty_tia@apple.com>
2017-10-20 15:27:33 +1100

    remove photoshop and some code cleanup

--- js/controllers/skill.js
+++ js/controllers/skill.js
@@ -17,20 +17,26 @@ app.controller('skillCtrl', ['$scope', function($scope){
         {label: 'bit bucket', category: 'version control'},
         {label: 'source tree', category: 'version control'},
         {label: 'git', category: 'version control'},
-        {label: 'photoshop', category: 'all'},
+        // {label: 'photoshop', category: 'all'},
         {label: 'sub version', category: 'version control'}
     ];
 
     $scope.convertButton = function(value, applyDot){
         if(value === 'all'){
             return '*';
+            
         } else {
             value = value.replace(' ', '-');
-            return applyDot ? '.' +value : value;
+
+            return applyDot 
+                ? '.' +value 
+                : value;
         }
     }
 
     $scope.getStyle = function(value){
+
+        // if scale prop valid, else return standard scale of 1,1
         value = value ? value +', ' +value : '1, 1';
         return 'transform: scale(' +value +')';
     }
@@ -51,6 +57,7 @@ app.controller('skillCtrl', ['$scope', function($scope){
 
         if(dataset){
 
+            // toggle active button state
             if (e.target.classList.contains('btn-category')){
 
                 $('.btn-category').removeClass('btn-danger');
