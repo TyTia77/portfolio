e2c38db4d900635cdb04f7b1b977d2de626b1fb7
Ty Tia <tytia77@gmail.com>
2017-11-23 16:11:49 +1100

    fix coding indentation

--- js/controllers/skill.js
+++ js/controllers/skill.js
@@ -1,7 +1,5 @@
 app.controller('skillCtrl', ['$scope', 'myService', (scope, myService) => {
     scope.items = myService.getSkillItems();
     scope.convertButton = (value, applyDot) => myService.convertButton(value, applyDot);
-    scope.handleClick = e => {
-        myService.handleButton('.skill-grid','.element-item')(e)('btn-skills');
-    }
+    scope.handleClick = e => myService.handleButton('.skill-grid','.element-item')(e)('btn-skills');
 }]);
